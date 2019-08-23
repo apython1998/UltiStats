b44ae87c572563382e444374cb0c36f1e469d4c8
apython1998 <alex.python1998@gmail.com>
2019-08-23 14:34:52 -0400

    Added all of API up to users

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -21,6 +21,9 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
 
@@ -36,7 +39,7 @@ def create_app(config_class=Config):
             mail_handler = SMTPHandler(
                 mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
                 fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                toaddrs=app.config['ADMINS'], subject='UltiStats Failure',
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
@@ -48,7 +51,7 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
+            file_handler = RotatingFileHandler('logs/ultistats.log',
                                                maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
                 '%(asctime)s %(levelname)s: %(message)s '
--- api/app/api/users.py
+++ api/app/api/users.py
@@ -21,33 +21,11 @@ def get_users():
     return jsonify(data)
 
 
-@bp.route('/users/<int:id>/followers', methods=['GET'])
-@token_auth.login_required
-def get_followers(id):
-    user = User.query.get_or_404(id)
-    page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = User.to_collection_dict(user.followers, page, per_page,
-                                   'api.get_followers', id=id)
-    return jsonify(data)
-
-
-@bp.route('/users/<int:id>/followed', methods=['GET'])
-@token_auth.login_required
-def get_followed(id):
-    user = User.query.get_or_404(id)
-    page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = User.to_collection_dict(user.followed, page, per_page,
-                                   'api.get_followed', id=id)
-    return jsonify(data)
-
-
 @bp.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
-        return bad_request('must include username, email and password fields')
+        return bad_request('Must include username, email and password fields')
     if User.query.filter_by(username=data['username']).first():
         return bad_request('please use a different username')
     if User.query.filter_by(email=data['email']).first():
