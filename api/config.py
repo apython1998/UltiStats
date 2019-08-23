edfa2e2a439803ff21427bd933acb2cc8d8b0a22
apython1998 <alex.python1998@gmail.com>
2019-08-23 13:50:52 -0400

    Edit config

--- api/config.py
+++ api/config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdf-4358dsalfn?!@'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_is_this'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
